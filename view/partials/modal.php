20e18dd63838b319ffd3b0daf32d425ccb439788
IvanAlexisUrbina <alexis-crokis@hotmial.com>
2023-02-26 22:21:58 -0500

    [feat][style]: se añade modal para utilizarla en varias vistas

--- view/partials/modal.php
+++ view/partials/modal.php
@@ -1,19 +1,8 @@
-<div class="modal" id="modal" tabindex="-1" role="dialog">
-
-<div class="modal-dialog" role="document">
-  <div class="modal-content"  class="modal-dialog modal-dialog-centered">
-    <div class="modal-header">
-      <h5 class="modal-title" id="titulo">Usuario</h5>
-      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
+<div id="modal" class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog"
+    aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+       
+        <div class="modal-content" id="contenedor"> 
+        </div>
     </div>
-    
-      <div style="padding:5px;" id="contenedor">
-
-      </div>
-    
-  </div>
-  
-</div>
 </div>
